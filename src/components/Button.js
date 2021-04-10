32c043977c1d7a465cc160ea39b39f76115579bd
Michaelkmt <michaelkmt.95@gmail.com>
2021-04-10 00:15:52 -0400

    React Project Created

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Button extends React.Component {
+	render() {
+		return (
+			<button onClick={this.props.choice} className={this.props.isLights ? 'light-button' : 'dark-button'}>
+				{this.props.children}
+			</button>
+		);
+	}
+}
+
+export { Button };
+//export default Button;
